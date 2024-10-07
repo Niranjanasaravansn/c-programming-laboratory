13623d01165db5149b52ceddd125a78d239ba71d
Niranjanasaravansn <niranjanasaravanan572@gmail.com>
2024-10-07 06:42:04 +0530

    main(4).c

---
+++ main (4).c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+
+{
+    int n;
+    printf("enter the Year:");
+    scanf("%d",&n);
+    
+    if(n%4==0)
+    {
+        printf("leap year");
+    }
+    if (n%4!=0)
+{
+    printf("not leap year");
+}
+    return 0; 
+}
