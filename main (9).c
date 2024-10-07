c0519d99048f162582f9dca9a9495dd84f734b19
Niranjanasaravansn <niranjanasaravanan572@gmail.com>
2024-10-07 07:32:44 +0530

    Add files via upload

---
+++ main (9).c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d\n%d",&a,&b);
+    
+    if (a==b)
+    {
+        printf("equal");
+    }
+    else
+    {
+        printf("not equal");
+    }
+    return 0;
+}
