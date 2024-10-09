9ad3bb884191544f940a241ade43f14562e9b611
Niranjanasaravansn <niranjanasaravanan572@gmail.com>
2024-10-09 21:07:10 +0530

    Add files via upload

---
+++ exp2-level 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    char upperchar,lowerchar;
+    printf("enter the upperchar:");
+    scanf("%c",&upperchar);
+    lowerchar=upperchar+32;
+    printf("\nlowerchar=%c",lowerchar);
+    
+ 
+     return 0;}
+    
+
+
+
+
