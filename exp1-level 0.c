55dc4da8bed9c77be30e65ab57ff085597f69521
Niranjanasaravansn <niranjanasaravanan572@gmail.com>
2024-10-09 21:03:58 +0530

    Add files via upload

---
+++ exp1-level 0.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int y,n1,n2,n3;
+    printf("y:");
+    scanf("%d",&y);
+    n1=1*y;
+    n2=2*y;
+    n3=3*y;
+    printf("%d\n%d\n%d",n1,n2,n3);
+     return 0;}
+    
+
+
+
+
+
+
