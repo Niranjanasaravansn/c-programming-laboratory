c7df28f14db8cf81bf03060475a6638285ecbb29
Niranjanasaravansn <niranjanasaravanan572@gmail.com>
2024-10-20 07:35:18 +0530

    Add files via upload

---
+++ exp2_level1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,e,i,o,u;
+    scanf("%d",&n);
+    if((n==a)||(n==e)||(n==i)||(n==o)||(n==u))
+    {
+        printf("vowel");
+        
+    }
+     
+    else
+    { 
+       printf("consonant");}
+        
+}
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
