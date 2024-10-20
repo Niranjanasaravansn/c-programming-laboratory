7db3b15da8c468d9bbe3e457983e88c4d0de6a78
Niranjanasaravansn <niranjanasaravanan572@gmail.com>
2024-10-20 07:35:54 +0530

    Add files via upload

---
+++ exp2-level3.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+
+{
+  char n;
+  scanf("%c",&n);
+  switch(n)
+  { 
+      case'E':
+      printf("Excellent");
+      break;
+      
+      case'v':
+      printf("very good");
+      break;
+      
+     case'G':
+      printf("good");
+      break;
+      
+      case'A':                                                       
+      printf("Average");
+      break;
+      
+      default:
+      printf("invalid Grade");
+      break;
+        
+}
+
+} 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
